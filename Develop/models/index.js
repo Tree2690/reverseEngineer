fc9839186f047da8496445bd0e8f8f1495f4bc91
Jonathan Liang <jonathanliang2690123@gmail.com>
2020-07-16 00:04:23 -0700

    added comment

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,13 +1,23 @@
 'use strict';
+// use strict - "cleans up javascript code"  = forces poor syntax practices to cause an error
 
 var fs        = require('fs');
+// brings in node modules "file save system"
 var path      = require('path');
+// brings in node module path, usage when joingin files or function
 var Sequelize = require('sequelize');
+//bring in node module sequelize, allows sequelize database
 var basename  = path.basename(module.filename);
+// dependent on the 'path' module system
 var env       = process.env.NODE_ENV || 'development';
+//brings in node module dot.env
+//allows programmer to hide certain pieces of data, ex. API keys
 var config    = require(__dirname + '/../config/config.json')[env];
+//bring in config
 var db        = {};
+//creating an empty object for db
 
+//setting up Sequelization
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
